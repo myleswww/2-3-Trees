108a0095bbe05a9f005d7854013d253cb33fe681
mbwright <myleswright0518@gmail.com>
2021-04-27 00:53:59 -0400

    ugh

--- twoThreeTreeMylesWright.cpp
+++ twoThreeTreeMylesWright.cpp
@@ -31,7 +31,7 @@ class Node{
     int getMaxVal();
     Node * getChild(int childToGet);
     int getValue(int valToGet);
-    int setParent(Node node);
+    void setParent(Node * node);
     void addVal(int val);
     void removeVal(int val);
 };
@@ -40,13 +40,22 @@ class Node{
         for(int i=0; i<6; i++){
             this->value[i] = 0;
         }
+        for(int i=0; i<6; i++){
+            cout << "Node value " << i << ": " << this->value[i] << endl;
+        }
     }
     Node::Node(int val){
         this->value[0] = val; //wtf is the point of having this length 6? I guess I will have to find out. Or google idk.
-        this->child[0] = 0;
         for(int i=1; i<6; i++){
             this->value[i] = 0;
         }
+        for(int i=0; i<6; i++){
+            cout << "Node+1 value " << i << ": " << this->value[i] << endl;
+        }
+    }
+    
+    void Node::setParent(Node * node){
+        this->parent = node;
     }
 
     Node * Node::getChild(int childToGet){
@@ -92,9 +101,10 @@ class Node{
             }
             else{
                 //do nothing
+                
             }
         }
-
+        cout << "Max function returns " << max << endl;
         return max;
     }
 
@@ -112,15 +122,18 @@ class Node{
 
 
     void Node::absorb(Node * newChild){
-         
+         cout << "In absorb" << endl;
          for(int i = 0; i < 3; i++){
-            if(this->child[i]->getValue(i) == 0){ //go until you hit a 0.
-                this->child[i] = newChild;  
-                return;
-            }
-            else{
-                //do nothing
-            }
+             cout << "in for loop" << endl;
+             cout << "the child value = " << this->child[i]->getValue(i);
+             if(this->child[i]->getValue(i) == 0){ //go until you hit a 0.
+                 //cout << "the child value = " << this->child[i]->getValue(i);
+                 this->child[i] = newChild;  
+                 
+             }
+                 //do nothing
+            
+             
         }
     }
 
@@ -179,7 +192,6 @@ class Tree{
             Node * oof = new Node(arr[i]);
             init[i] = *oof;
         } 
-
         /*Then, assign each one a parent node, and shove the values into the parent node (will be either 2 or 3 values, I am following Uzi's diagram for this.) 
           Actually I lied I'm using the same numbers but organizing the tree differently because I really don't enjoy how he has it. It's dumb, like take every 3 values, shove them into
           a node and if youre left with one value at the end, move one of the other numbers over so the last two nodes have 2 values its not that hard jesus and its the exact same height like get a grip Uzi
@@ -211,36 +223,51 @@ class Tree{
 
 
     void Tree::buildTree(Node * leaves, int size){
+        cout << "Inside buildTree" << endl;
         int sizeround = ceil(size/3);
         int itemstravelled = 0;
         int itemsleft = size;
         Node * p1 = new Node[sizeround];
+
+        for(int i = 0; i < sizeround; i++){
+            Node * oof = new Node();
+            p1[i] = *oof;
+        }
+
         for(int j = 0; j < sizeround; j++){ //OKAY so basically this is going to have itemsleft and if it is >=3 all items get added to parent node, if it is =2, only those ones are added, if it is =1, then the last item in the node before gets removed and added to make it 2 ya feel?
-            //p1[j] = new Node()*;
+            cout << "In for loop" << endl;
             if(itemsleft >= 3){
+                cout << "Items left:" << itemsleft << endl;
                 for(int h = 0; h < 3; h++){
+                    cout << "Items traversed:" << itemstravelled << endl;
                     p1[j].addVal(leaves[itemstravelled].getMaxVal());
-                    leaves[itemstravelled].setParent(p1[j]); //setting connections for two way traversal
+                    cout << "Added leaf value" << p1[j] endl;
+                    leaves[itemstravelled].setParent(&p1[j]); //setting connections for two way traversal
+                    cout << "Set Parent" << endl;
                     p1[j].absorb(&leaves[itemstravelled]);
+                    cout << "absorbed" <<  endl;
                     itemstravelled++;
                 }
                 itemsleft -= 3;
             }
             if(itemsleft == 2){
+                cout << "Items left:" << itemsleft << endl;
                 for(int h = 0; h < 2; h++){
+                    cout << "Items traversed:" << itemstravelled << endl;
                     p1[j].addVal(leaves[itemstravelled].getMaxVal());
-                    leaves[itemstravelled].setParent(p1[j]); //setting connections for two way traversal
+                    leaves[itemstravelled].setParent(&p1[j]); //setting connections for two way traversal
                     p1[j].absorb(&leaves[itemstravelled]);
                     itemstravelled++;
                 }
                 itemsleft -= 2;
             }
             if(itemsleft == 1){ //this is the shitty one. have to go to previous p node and remove an element, then insert it into pi then insert the last element.
+                cout << "Items left:" << itemsleft << endl;
                 p1[j-1].discard(&leaves[itemstravelled-1]); //removing previous value.
                 p1[j].addVal(leaves[itemstravelled-1].getMaxVal()); //inserting it into pi.
                 p1[j].addVal(leaves[itemstravelled].getMaxVal()); //inserting current element into pi
-                leaves[itemstravelled].setParent(p1[j-1]);
-                leaves[itemstravelled].setParent(p1[j]); //setting connections for two way traversal
+                leaves[itemstravelled].setParent(&p1[j-1]);
+                leaves[itemstravelled].setParent(&p1[j]); //setting connections for two way traversal
                 p1[j].absorb(&leaves[itemstravelled]);
 
             }
@@ -248,10 +275,12 @@ class Tree{
 
         //Now we get to do this all over again, but instead of the leaves, use the level we just made, and the size of the level which is always round(size/3) :)
         if(sizeround > 1){
+            cout << "Recursing" << endl;
             buildTree(p1, sizeround);
         }
         else{
             //you are done. set the root node and be done with it.
+            cout << "At root node" << endl;
             this->root = &p1[0];
         }
     }
@@ -336,14 +365,14 @@ class Tree{
 
 
 
-int main(int argc, char * argv[]){
+int main(){
     //first we need to build this little shit.
     int size = 15;
     int seq[size] = {1, 3, 5, 8, 11, 14, 17, 19, 21, 24, 28, 31, 33, 36, 39};
-
+    cout << "Making tree" << endl;
     //and now we pass it to the helper function I made because why do this all in main???? thats dumb.
     Tree * tr = new Tree(seq, size);
-
+    cout << "Made tree" << endl;
     tr->print();
 
 }
